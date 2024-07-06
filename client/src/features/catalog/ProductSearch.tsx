c30b0da218d531b66178f37d38fb7a4c01c5aab7
Myroslav Mazurenko <mazurenkomyroslav@gmail.com>
2024-07-06 13:37:15 +0300

    End of section 8

---
+++ client/src/features/catalog/ProductSearch.tsx
@@ -0,0 +1,30 @@
+import { debounce, TextField } from "@mui/material";
+import { useAppSelector } from "../../app/store/configureStore";
+import { useDispatch } from "react-redux";
+import { setProductParams } from "./catalogSlice";
+import { useState } from "react";
+
+export default function ProductSearch() {
+    const { productParams } = useAppSelector(state => state.catalog);
+    const [searchTerm, setSearchTerm] = useState(productParams.searchTerm);
+    const dispatch = useDispatch();
+
+    const debouncedSearch = debounce((event: any) => {
+        dispatch(setProductParams({ searchTerm: event.target.value }))
+    }, 1000)
+
+    return (
+        <>
+            <TextField
+                label="Search products"
+                variant="outlined"
+                fullWidth
+                value={searchTerm || ""}
+                onChange={(event: any) => {
+                    setSearchTerm(event.target.value);
+                    debouncedSearch(event);
+                }}
+            />
+        </>
+    )
+}
