c9657c81dd9914cd87f54308515271c9746ed141
Myroslav Mazurenko <mazurenkomyroslav@gmail.com>
2024-07-21 20:07:10 +0300

    Inventory Complete

--- client/src/app/router/Routes.tsx
+++ client/src/app/router/Routes.tsx
@@ -12,6 +12,7 @@ import Login from '../../features/account/Login';
 import Register from '../../features/account/Register';
 import RequireAuth from './RequireAuth';
 import Orders from '../../features/orders/Orders';
+import Inventory from '../../features/admin/Inventory';
 
 export const router = createBrowserRouter(([
     {
@@ -20,19 +21,25 @@ export const router = createBrowserRouter(([
         children: [
             {
                 element: <RequireAuth />, children: [
-                    { path: '/checkout', element: <CheckoutPage /> },
-                    { path: '/orders', element: <Orders /> },
+                    { path: 'checkout', element: <CheckoutPage /> },
+                    { path: 'orders', element: <Orders /> },
+                    { path: 'inventory', element: <Inventory /> },
+                ]
+            },
+            {
+                element: <RequireAuth roles={['Admin']} />, children: [
+                    { path: 'inventory', element: <Inventory /> },
                 ]
             },
             { path: 'catalog', element: <Catalog /> },
             { path: 'catalog/:id', element: <ProductDetails /> },
             { path: 'about', element: <AboutPage /> },
             { path: 'contact', element: <ContactPage /> },
-            { path: '/server-error', element: <ServerError /> },
-            { path: '/not-found', element: <NotFound /> },
-            { path: '/basket', element: <BasketPage /> },
-            { path: '/login', element: <Login /> },
-            { path: '/register', element: <Register /> },
+            { path: 'server-error', element: <ServerError /> },
+            { path: 'not-found', element: <NotFound /> },
+            { path: 'basket', element: <BasketPage /> },
+            { path: 'login', element: <Login /> },
+            { path: 'register', element: <Register /> },
             { path: '*', element: <Navigate replace to='/not-found' /> },
         ]
     }
